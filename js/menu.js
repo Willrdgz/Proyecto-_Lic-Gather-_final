dbf84328d84ae9a949aed1e74c26e82598bdcf31
Willrdgz <137135562+Willrdgz@users.noreply.github.com>
2025-08-30 21:33:21 -0600

    Delete js directory

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-// Recuperar usuario de localStorage
-  let usuario = localStorage.getItem("usuarioLogeado");
-
-  if (usuario) {
-    document.getElementById("nombreUsuario").textContent = usuario;
-  } else {
-    document.getElementById("nombreUsuario").textContent = "Invitado";
-  }
